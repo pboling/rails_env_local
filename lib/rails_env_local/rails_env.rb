6a06e2a72341d0e1c8c320ed8612be13bc8d9874
Peter Boling <peter.boling@gmail.com>
2016-03-11 09:43:31 +0700

    Adds warning for environment name "local"

--- lib/rails_env_local/rails_env.rb
+++ lib/rails_env_local/rails_env.rb
@@ -3,10 +3,18 @@ module RailsEnvLocal
   module RailsEnv
     def env
       result = super
-      if result == "development"
+      case result
+      when "development" then
         msg =  %[WARNING: environment is incorrectly "development". Early access of Rails.env might be the cause, consider moving `RailsEnvLocal.set_local_environment` up in config/boot.rb]
         warn(msg)
         Rails.logger.error(msg) if Rails.logger
+      when "local"
+        msg =  %[WARNING: environment name "local" is partly incompatible with dotenv gem, and probably others]
+        warn(msg)
+        Rails.logger.error(msg) if Rails.logger
+      else
+        # If other bad environment names are found add them to the case statement.
+        # Share the knowledge about environment name incompatibility!
       end
       result
     end
